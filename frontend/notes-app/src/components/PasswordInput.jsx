750f77a47d5dc61ad19a9cae8363565fafe311c3
Pablo Valladares González <PValladaresG@proton.me>
2026-03-18 21:42:46 +0100

    Backend

--- frontend/notes-app/src/components/PasswordInput.jsx
+++ frontend/notes-app/src/components/PasswordInput.jsx
@@ -8,6 +8,7 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
     const toggleShowPassword = () => {
         setIsShowPassword(prev => !prev);
     };
+    
     return (
         <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
             <input
